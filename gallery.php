fe1c0928a277b02abe5f01700159b680f642e588
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-15 18:45:22 +0100

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -1,7 +1,14 @@
 <?php
 require "header.php";
  ?>
-
+ <main>
+   <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
+    <div class="container">
+      <h2>“My protein shake brings the boys to the yard. And they’re like:
+        your muscles are hard. Damn right, my muscles are hard. I could train ya, but I’d have to charge.”</h2>
+    </div>
+  </div>
+ </main>
  <?php
 require "footer.php";
   ?>
