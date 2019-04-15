b7e0588d5463e7d7db39dd625dda47dc2f2698cd
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-15 23:30:20 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,31 +1,9 @@
 <?php
 require "header.php";
  ?>
- <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Material Design Bootstrap</title>
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
-    <!-- Material Design Bootstrap -->
-    <link rel="stylesheet" href="css/mdb.css">
-    <!-- Your custom styles (optional) -->
-    <style>
-
-    </style>
-</head>
 
 <body>
     <div class="container">
-
-
-
 
         <!--Section: Contact v.2-->
         <section class="section">
