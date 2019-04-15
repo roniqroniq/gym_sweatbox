4eddf8ec56dbfe94860d26c171ce8ddca371a6c2
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-15 20:25:00 +0100

    Update header.php

--- header.php
+++ header.php
@@ -6,17 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
     <link rel="stylesheet" type="text/css" href="theme.css">
-    
-    
+
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    
-    
+
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-  
-    
+
+
     <link href="https://fonts.googleapis.com/css?family=Archivo:500" rel="stylesheet">
 <body>
 
@@ -26,35 +26,35 @@
         <a class="navbar-brand" href="index.php" style="max-width: 4%;">
                 <img src="logo.png" class="img-fluid">
              </a>
-        
-          
+
+
           <!-- links toggle -->
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#links" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          
-  
+
+
           <div class="collapse navbar-collapse" id="links">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="#">ABOUT</a>
+                <a class="nav-link" href="about.php">ABOUT</a>
               </li>
               <li class="nav-item active">
-                <a class="nav-link" href="#">GALLERY</a>
+                <a class="nav-link" href="gallery.php">GALLERY</a>
               </li>
               <li class="nav-item active">
-                <a class="nav-link" href="#">CLASSES</a>
+                <a class="nav-link" href="classes.php">CLASSES</a>
               </li>
               <li class="nav-item active">
-                <a class="nav-link" href="#">JOIN NOW</a>
+                <a class="nav-link" href="signup.php">JOIN NOW</a>
               </li>
-             
+
               <li class="nav-item  active dropdown">
                   <a class="nav-link dropdown-toggle" id=""href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     MEMBERSHIP
                   </a>
                   <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                    <a class="dropdown-item" href="#">TESTIMONIALS</a>
+                    <a class="dropdown-item" href="testimonial.php">TESTIMONIALS</a>
                     <a class="dropdown-item" href="#">FORUM</a>
                     <a class="dropdown-item" href="events.php">EVENTS</a>
                   </div>
@@ -63,20 +63,20 @@
             </div>
             <div class="collapse navbar-collapse" id="account">
             <ul class="navbar-nav ml-auto">
-             
+
               <li class="nav-item active"><a class="nav-link" href="#">LOG IN</a></li>
             </ul>
           </div>
         </div>
       </nav>
     </div>
-   
-    
-        
+
+
+
        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
-  
+
       </body>
 </html>
