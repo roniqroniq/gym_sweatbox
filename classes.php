391686dccfa8c623f8bbe63f1e077b6d98eb6dfc
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-15 19:45:12 +0100

    Update classes.php

--- classes.php
+++ classes.php
@@ -4,7 +4,7 @@ require "header.php";
  <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
     <div class="container">
       <h2>“Training gives us an outlet for suppressed energies created by
-        stress and thus tones the spirit just as exercise conditions the body.”</h2>
+        stress and thus tones the spirit just as exercise conditions the body...”</h2>
     </div>
   </div>
  <?php
