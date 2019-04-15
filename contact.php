348d5bf0621c1eeca17b44f47923553af85c8026
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-15 18:45:17 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,7 +1,13 @@
 <?php
 require "header.php";
  ?>
-
+ <main>
+   <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
+    <div class="container">
+      <h2>“Strength doesn’t come from what you can do. It comes from overcoming the things you once thought you couldn’t.”</h2>
+    </div>
+  </div>
+ </main>
  <?php
 require "footer.php";
   ?>
