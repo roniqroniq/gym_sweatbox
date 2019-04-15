01579de610d438ba32778d1e9b5c85b752845344
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-15 20:25:05 +0100

    Update events.php

--- events.php
+++ events.php
@@ -2,7 +2,13 @@
 include "header.php";
 ?>
 
-
+<main>
+  <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
+   <div class="container">
+     <h2>“Those who think they have not time for bodily exercise will sooner or later have to find time for illness.”</h2>
+   </div>
+ </div>
+</main>
 <p>Hello</p>
 
     Hello there everyone
