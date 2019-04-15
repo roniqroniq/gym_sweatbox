d08b73ea00bfd4b763f90a50ca819625c059582f
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-15 18:45:31 +0100

    Create testimonial.php

---
+++ testimonial.php
@@ -0,0 +1,14 @@
+<?php
+require "header.php";
+ ?>
+ <main>
+   <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
+    <div class="container">
+      <h2>“Nothing comes easy, but as long as you’re breathing, you’re always one breath away from making your dreams a reality. Make every breath count.”</h2>
+    </div>
+  </div>
+ </main>
+
+ <?php
+require "footer.php"
+  ?>
