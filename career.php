0234daa9bc1406e05d2e38e6b1c213c6f1c76e0d
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-15 18:45:07 +0100

    Create career.php

---
+++ career.php
@@ -0,0 +1,11 @@
+<?php
+require "header.php";
+ ?>
+ <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
+    <div class="container">
+      <h2>“Strength does not come from the physical capacity. It comes from an indomitable will.”</h2>
+    </div>
+  </div>
+ <?php
+require "footer.php";
+  ?>
