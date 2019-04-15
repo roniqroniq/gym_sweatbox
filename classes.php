a4b7a387f9ce5930a16882e82da6c8afd707306c
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-15 22:52:56 +0100

    Update classes.php

--- classes.php
+++ classes.php
@@ -1,7 +1,7 @@
 <?php
 require "header.php";
  ?>
- 
+<body>
  <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
     <div class="container">
       <h2>“Training gives us an outlet for suppressed energies created by
@@ -9,7 +9,41 @@ require "header.php";
       </div>
     </div>
   </div>
-
+  <table class="table table-striped table-dark">
+    <thead>
+      <tr>
+        <th scope="col">What</th>
+        <th scope="col">Monday & Wednesday</th>
+        <th scope="col">Tuesday & Friday</th>
+        <th scope="col">Thursday & Saturday</th>
+  	  <th scope="col">Sunday</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">Spin</th>
+        <td>10:00 - 11:00</td>
+        <td>12:30 - 13:30</td>
+        <td>14:00 - 15:00</td>
+  	  <td>16:30 - 17:30</td>
+      </tr>
+      <tr>
+        <th scope="row">Bodyflex</th>
+        <td>10:00 - 11:00</td>
+        <td>12:30 - 13:30</td>
+  	  <td>12:30 - 13:30</td>
+        <td>15:00 - 16:00</td>
+      </tr>
+      <tr>
+        <th scope="row">Kettlebell</th>
+        <td>10:00 - 11:00</td>
+  	  <td>12:30 - 13:30</td>
+        <td>12:30 - 13:30</td>
+        <td>15:30 - 16:30</td>
+      </tr>
+    </tbody>
+  </table>
+</body>
  <?php
 require "footer.php";
   ?>
