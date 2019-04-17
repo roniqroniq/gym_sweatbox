45dc263d064950ead96b2f6744905ef291ad91ca
Owen Lynch <owenlynch1310@gmail.com>
2019-04-17 17:52:40 +0100

    Add files via upload

---
+++ events.php
@@ -0,0 +1,94 @@
+<?php
+include "header.php";
+?>
+<html>
+<body>
+<div id ="banner">
+  <div class="jumbotron jumbotron-fluid" style="background-image: url(gym-banner.jpg); background-size: cover; color: white;">
+   <div class="container">
+       <br>
+     <h2>“Those who think they have not time for bodily exercise will sooner or later have to find time for illness.”</h2>
+   </div>
+ </div>
+</div>
+    <h1 class="display-6"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">Events at Sweat Box</h1>
+    <br>
+    
+
+<br>
+<br>
+<br>   
+    <div id="backgroundimage1"></div>
+    <section>
+    <div class="container ">
+    <div class="row">
+    	<div class="card col-md-12 p-3 ">
+    		<div class="row ">
+    			<div class="col-md-4">
+    				<img class="w-100" src="focus.jpg" style ="width:500px;height:250px;margin:5%;">
+    			</div>
+    			<div class="col-md-8">
+    				<div class="card-body">
+    					<h6 class="card-title text-center border-0">WEEKLY EVENTS</h6>
+              <div id="text">
+    					<p class="card-text  ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    					<a href="https://www.google.com" class="btn"style="background-color:#5454">More.</a>
+                 </div>   
+             </div>
+    			</div>
+         
+    		</div>
+        </div>
+        <div class="card">
+    	<div class="card col-md-12 p-3">
+      
+    		<div class="row ">
+            <div class="col-md-4">
+    				<img class="w-100" src="adult.jpg"style ="width:500px;height:250px;margin:5%;">
+    			</div>
+    			<div class="col-md-8">
+    				<div class="card-body">
+    					<h6 class="card-title text-center">MONTHLY EVENTS</h6>
+              <div id ="text">
+    					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    					<a href="#" class="btn" style="background-color:#5454">More.</a>
+              </div>
+    				</div>
+    			</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<section>
+</section>
+<script>
+
+$( window ).on( "load", function() {
+    mainNav();
+    $(window).scroll(function() {
+       mainNav();
+    });
+    function mainNav() {
+        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+       if (top > 1) $('.navy').stop().animate({
+          "opacity": '0',
+            "top": '0'
+         });
+       else $('.navy').stop().animate({
+            "top": '1',
+           "opacity": '1'
+       });
+
+ }
+     });
+   
+
+
+
+</script>
+</body>
+</html>
+<?php
+include "footer.php";
+?>
---
+++ index.php
@@ -0,0 +1,178 @@
+<?php
+include "header.php";
+?>
+
+
+
+<html>
+  
+    
+<body>  
+
+<div class="box" style="position: relative; left: 0; top: 0;">
+<img src="kettlebell.jpg" class="gym"/>
+    <img src="logo.png" class="log"/>
+</div>
+
+<br>
+
+<section>
+    <div class="container ">
+    <div class="row">
+    	<div class="card col-md-12 p-3 ">
+    		<div class="row ">
+    			<div class="col-md-4">
+    				<img class="w-100" src="aboutus.jpg" style ="width:500px;height:250px;margin:5%;">
+    			</div>
+    			<div class="col-md-8">
+    				<div class="card-body">
+    					<h6 class="card-title text-center border-0"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">ABOUT US</h6>
+              <div class ="text-center">
+    					<p class="card-text  ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    					<a href="https://www.google.com" class="btn"style="background-color:#ADFF2F">More.</a>
+                 </div>   
+             </div>
+    			</div>
+         
+    		</div>
+        </div>
+        <div class="card">
+    	<div class="card col-md-12 p-3">
+      
+    		<div class="row ">
+            <div class="col-md-4">
+    				<img class="w-100" src="girls_jump.jpg"style ="width:500px;height:250px;margin:5%;">
+    			</div>
+    			<div class="col-md-8">
+    				<div class="card-body">
+    					<h6 class="card-title text-center"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">JOIN NOW</h6>
+              <div class="text-center">
+    					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    					<a href="#" class="btn" style="background-color:#ADFF2F">JOIN</a>
+              </div>
+    				</div>
+    			</div>
+</div>
+</div>
+</div>
+</div>
+
+<br>
+<br>
+
+<section class="services pt-100 pb-50" id="services">
+         <div class="container">
+            <div class="row">
+               <div class="col-xl-6 mx-auto text-center">
+                  <div class="section-title mb-100"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                     <h2>WHAT WE OFFER</h2>
+                   <br>
+                   <br>
+                   <br>
+                   
+                  </div>
+               </div>
+            </div>
+            <br>
+            <br>
+            
+            <div class="row">
+               <div class="col-lg-4 col-md-6">
+                  <!-- Single Service -->
+                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                     <i class="fa fa-laptop"></i>
+                     <h4>3 FLOORS  </h4>
+                     <p>At sweat box we have three floors and each floor containing different sections to train</p>
+                  
+                  </div>
+
+               </div>
+               <br> 
+               <br>
+               <div class="col-lg-4 col-md-6">
+                  <!-- Single Service -->
+                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                      <i class="fa fa-gears"></i> 
+                     <h4> EQUIPMENT </h4>
+                     <p>At sweat box we have world class equipment that is maintained and updated every year. </p>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6">
+                  <!-- Single Service -->
+                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                  <i class="fa fa-swimmer"></i>
+                     <h4>POOL</h4>
+                     <p>We have a 25 meter pool as well as a steam room ,sauna and jacuzzi </p>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6">
+                  <!-- Single Service -->
+                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                      <i class="fa fa-magic"></i> 
+                     <h4>MINDFULLNESS</h4>
+                     <p>We have a special mindfullness area where you can go to meditate and take time for you </p>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6">
+                  <!-- Single Service -->
+                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                      <i class="fa fa-pencil"></i> 
+                     <h4>INSTRUCTORS</h4>
+                     <p> Fully trained instructors in both strenght and conditioning and nutrition</p>
+                  </div>
+               </div>
+               <div class="col-lg-4 col-md-6">
+                  <!-- Single Service -->
+                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                     <i class="fa fa-lightbulb-o"></i> 
+                     <h4>PLAN</h4>
+                     <p>Not only will you get a personailsed excersise plan you will also get a nutrional review<p>
+               </div>
+            </div>
+         </div>
+         <div class="text-center">
+    					<a href="#" class="btn" style="background-color:#ADFF2F">JOIN</a>
+              </div>
+      </section>
+      </div>
+<br>
+<div class="jumbotron jumbotron-fluid"style="font-family: 'Roboto', sans-serif,!important;color:grey">
+  <div class="container">
+    <h1 class="display-4">"IF YOU FEEL LIKE STOPPING , THINK ABOUT WHY YOU STARTED."</h1>
+   
+  </div>
+</div>
+</div>
+</div>
+
+<script>
+
+$( window ).on( "load", function() {
+    mainNav();
+    $(window).scroll(function() {
+       mainNav();
+    });
+    function mainNav() {
+        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+       if (top > 600) $('.navy').stop().animate({
+          "opacity": '0',
+            "top": '0'
+         });
+       else $('.navy').stop().animate({
+            "top": '1',
+           "opacity": '1'
+       });
+
+ }
+     });
+   
+
+
+
+</script>
+</body>
+</html>
+
+<?php
+include "footer.php";
+?>
