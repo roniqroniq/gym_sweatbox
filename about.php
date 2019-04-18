ff038402d89217a20f1603b1ab562332db888945
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-18 08:59:42 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,14 @@
+<?php
+require "header.php";
+ ?>
+ <body>
+ <main>
+   <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
+    <div class="container">
+      <h2>“The hardest lift of all is lifting your butt off the couch.”</h2>
+    </div>
+  </div>
+
+  <?php
+ require "footer.php";
+   ?>
