aeccbf422f2877562fbb6aaa97532b6224d36cf8
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-18 21:27:03 +0100

    Update login.php

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+require "header.php";
+ ?>
+ <body>
+ <main>
+   <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
+    <div class="container">
+      <h2>“i like to meditade. But I get rid of my stress at the gym.”</h2>
+    </div>
+  </div>
+
+  <?php
+ require "footer.php";
+   ?>
