4de159a38a1fa3a72b73423f0de15e3667de64b3
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-18 21:22:54 +0100

    Update classes.php

--- classes.php
+++ classes.php
@@ -22,7 +22,7 @@ require "header.php";
                 </a>
               </div>
               <div class="media-body">
-                <h4 class="media-heading">Media heading</h4>
+                <h4 class="media-heading">Yoga</h4>
                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
               </div>
             </div>
@@ -35,7 +35,7 @@ require "header.php";
                 </a>
               </div>
               <div class="media-body">
-                <h4 class="media-heading">Media heading</h4>
+                <h4 class="media-heading">Boxingyoga</h4>
                 Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
               </div>
             </div>
@@ -52,7 +52,7 @@ require "header.php";
             </a>
           </div>
           <div class="media-body">
-            <h4 class="media-heading">Media heading</h4>
+            <h4 class="media-heading">Kettlebell</h4>
             Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
           </div>
         </div>
@@ -65,47 +65,73 @@ require "header.php";
             </a>
           </div>
           <div class="media-body">
-            <h4 class="media-heading">Media heading</h4>
+            <h4 class="media-heading">Spin Classes</h4>
             Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
           </div>
         </div>
     </div>
 </div>
 </div>
-  <table class="table table-striped table-dark">
-    <thead>
-      <tr>
-        <th scope="col">What</th>
-        <th scope="col">Monday & Wednesday</th>
-        <th scope="col">Tuesday & Friday</th>
-        <th scope="col">Thursday & Saturday</th>
-  	  <th scope="col">Sunday</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">Spin</th>
-        <td>10:00 - 11:00</td>
-        <td>12:30 - 13:30</td>
-        <td>14:00 - 15:00</td>
-  	  <td>16:30 - 17:30</td>
-      </tr>
-      <tr>
-        <th scope="row">Bodyflex</th>
-        <td>10:00 - 11:00</td>
-        <td>12:30 - 13:30</td>
-  	  <td>12:30 - 13:30</td>
-        <td>15:00 - 16:00</td>
-      </tr>
-      <tr>
-        <th scope="row">Kettlebell</th>
-        <td>10:00 - 11:00</td>
-  	  <td>12:30 - 13:30</td>
-        <td>12:30 - 13:30</td>
-        <td>15:30 - 16:30</td>
-      </tr>
-    </tbody>
-  </table>
+<div class="container">
+<div class="row">
+    <div class="col-md-6">
+        <div class="media">
+          <div class="media-left">
+            <a href="#">
+              <img class="media-object" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjEzLjQ2MDkzNzUiIHk9IjMyIiBzdHlsZT0iZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQ7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NjR4NjQ8L3RleHQ+PC9nPjwvc3ZnPg==" alt="...">
+            </a>
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Weight training</h4>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+          </div>
+        </div>
+    </div>
+    <div class="col-md-6">
+        <div class="media">
+          <div class="media-left">
+            <a href="#">
+              <img class="media-object" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjEzLjQ2MDkzNzUiIHk9IjMyIiBzdHlsZT0iZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQ7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NjR4NjQ8L3RleHQ+PC9nPjwvc3ZnPg==" alt="...">
+            </a>
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Pilates</h4>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+          </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+<div class="col-md-6">
+    <div class="media">
+      <div class="media-left">
+        <a href="#">
+          <img class="media-object" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjEzLjQ2MDkzNzUiIHk9IjMyIiBzdHlsZT0iZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQ7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NjR4NjQ8L3RleHQ+PC9nPjwvc3ZnPg==" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">Boxing</h4>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+      </div>
+    </div>
+</div>
+<div class="col-md-6">
+    <div class="media">
+      <div class="media-left">
+        <a href="#">
+          <img class="media-object" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PGRlZnMvPjxyZWN0IHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgZmlsbD0iI0VFRUVFRSIvPjxnPjx0ZXh0IHg9IjEzLjQ2MDkzNzUiIHk9IjMyIiBzdHlsZT0iZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQ7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NjR4NjQ8L3RleHQ+PC9nPjwvc3ZnPg==" alt="...">
+        </a>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">Have a individual class with an Instructor</h4>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.
+      </div>
+    </div>
+</div>
+</div>
+</div>
 </body>
  <?php
 require "footer.php";
