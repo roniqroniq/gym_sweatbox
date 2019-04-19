449491a2c71d3699239a698bbbbdb23dcaed93a9
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-19 12:52:19 +0100

    Update about.php

--- about.php
+++ about.php
@@ -9,6 +9,48 @@ require "header.php";
     </div>
   </div>
 
+  <section>
+      <div class="container ">
+      <div class="row">
+      	<div class="card col-md-12 p-3 ">
+      		<div class="row ">
+      			<div class="col-md-4">
+      				<img class="w-100" src="aboutus.jpg" style ="width:500px;height:250px;margin:5%;">
+      			</div>
+      			<div class="col-md-8">
+      				<div class="card-body">
+      					<h6 class="card-title text-center border-0"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">ABOUT US</h6>
+                <div class ="text-center">
+      					<p class="card-text  ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      					<a href="https://www.google.com" class="btn"style="background-color:#ADFF2F">More.</a>
+                   </div>
+               </div>
+      			</div>
+
+      		</div>
+          </div>
+          <div class="card">
+      	<div class="card col-md-12 p-3">
+
+      		<div class="row ">
+              <div class="col-md-4">
+      				<img class="w-100" src="girls_jump.jpg"style ="width:500px;height:250px;margin:5%;">
+      			</div>
+      			<div class="col-md-8">
+      				<div class="card-body">
+      					<h6 class="card-title text-center"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">JOIN NOW</h6>
+                <div class="text-center">
+      					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      					<a href="#" class="btn" style="background-color:#ADFF2F">JOIN</a>
+                </div>
+      				</div>
+      			</div>
+  </div>
+  </div>
+  </div>
+  </div>
+
+
   <?php
  require "footer.php";
    ?>
