f53802f0cfd62f816fd8cb7c47d29b26fddadadd
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-19 13:47:47 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ require "header.php";
  ?>
  <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
     <div class="container">
-      <h2>“Strength does not come from the physical capacity. It comes from an indomitable will.”</h2>
+      <h2>“FUCK YOU, BARRY!”</h2>
     </div>
   </div>
  <?php
