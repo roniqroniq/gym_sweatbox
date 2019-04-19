e9c7f6f4d84f5677e95b805bd959f9a130060eff
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-19 21:13:01 +0100

    Update login.php

--- login.php
+++ login.php
@@ -8,7 +8,32 @@ require "header.php";
       <h2>“I like to meditade. But I get rid of my stress at the gym.”</h2>
     </div>
   </div>
-
+  <body >
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h2>Contact Us</h2> Please log in using the form below. </p>
+                <form role="form" method="post" id="login_form" >
+                    <div class="form-group">
+                        <label for="name"> Name:</label>
+                        <input type="text" class="form-control" id="name" name="name" required maxlength="50">
+                    </div>
+                    <div class="form-group">
+                        <label for="email"> Email:</label>
+                        <input type="email" class="form-control" id="email" name="email" required maxlength="50">
+                    </div>
+                    <div class="form-group">
+                        <label for="name"> Message:</label>
+                        <textarea class="form-control" type="textarea" name="message" id="message" placeholder="Your Message Here" maxlength="6000" rows="7"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-lg btn-success pull-right" id="btnContactUs"a href="#" style="background-color:#ADFF2F" >Send &rarr;</button>
+                </form>
+                <div id="success_message" style="width:100%; height:100%; display:none; "> <h3>Sent your message successfully!</h3> </div>
+                <div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div>
+            </div>
+        </div>
+    </div>
+</body>
   <?php
  require "footer.php";
    ?>
