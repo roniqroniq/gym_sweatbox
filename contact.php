bd0a1638a25e0daa451c892b6f4b9829e80edf89
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-19 18:51:31 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ require "header.php";
  ?>
  <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
     <div class="container">
-      <h2>“FUCK YOU, BARRY!”</h2>
+      <h2>“Work hard in silence. Let success be your noise.”</h2>
     </div>
   </div>
   <body >
