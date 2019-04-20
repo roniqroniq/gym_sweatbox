335545347e5b3420da20fdbd041f127fd30c4c67
Owen Lynch <owenlynch1310@gmail.com>
2019-04-20 12:30:49 +0100

    Update events.php

--- events.php
+++ events.php
@@ -22,25 +22,27 @@ include "header.php";
     <section>
     <div class="container ">
     <div class="row">
-    	<div class="card col-md-12 p-3 ">
+    	<div class="card border-0 col-md-12 p-3 ">
     		<div class="row ">
     			<div class="col-md-4">
     				<img class="w-100" src="focus.jpg" style ="width:500px;height:250px;margin:5%;">
     			</div>
     			<div class="col-md-8">
     				<div class="card-body">
-    					<h6 class="card-title text-center border-0">WEEKLY EVENTS</h6>
+    					<h6 class="card-title text-center border-0"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">WEEKLY EVENTS</h6>
               <div id="text">
-    					<p class="card-text  ">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    					<a href="https://www.google.com" class="btn"style="background-color:#5454">More.</a>
-                 </div>   
+    					<p class="card-text"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <div class="text-center">
+              <a href="https://www.google.com" class="btn"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:white;background-color:grey;display:in-line;">More</a>
+</div>
+            </div>   
              </div>
     			</div>
          
     		</div>
         </div>
-        <div class="card">
-    	<div class="card col-md-12 p-3">
+        <div class="card border-0">
+    	<div class="card border-0 col-md-12 p-3">
       
     		<div class="row ">
             <div class="col-md-4">
@@ -48,11 +50,13 @@ include "header.php";
     			</div>
     			<div class="col-md-8">
     				<div class="card-body">
-    					<h6 class="card-title text-center">MONTHLY EVENTS</h6>
+    					<h6 class="card-title text-center"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">MONTHLY EVENTS</h6>
               <div id ="text">
-    					<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    					<a href="#" class="btn" style="background-color:#5454">More.</a>
-              </div>
+    					<p class="card-text"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <div class="text-center">
+              <a href="#" class="btn " style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:white;background-color:grey">More</a>
+</div>
+            </div>
     				</div>
     			</div>
 </div>
