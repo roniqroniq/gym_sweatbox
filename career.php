36515a033905b1d5023150195c9500a1b4a9f459
Miriam W <mirwalsh3@gmail.com>
2019-04-20 16:36:32 +0100

    change

--- career.php
+++ career.php
@@ -6,6 +6,77 @@ require "header.php";
       <h2>“Strength does not come from the physical capacity. It comes from an indomitable will.”</h2>
     </div>
   </div>
+
+  <div class="under-border" style="background: #CFDE00"></div>
+  	<div class="under-border under-border--opaic" style="background: #CFDE00"></div>
+
+
+  <section class="container">
+
+	<h2 class="text-center purple">Career Paths</h2>
+		<div class="row pd-t pd-t--md">
+
+
+				<div class="col-md-4 jobs">
+					<a href="https://gymplus.ie/jobs/front-of-house/">
+					<div class="job-card">
+						<h6 class="normal purple">
+							<i class="fa fa-location-arrow" aria-hidden="true"></i>
+							All Locations
+						</h6>
+						<h5>Front of House</h5>						<h6 class="normal">€ POA</h6>
+						<span class="gp-btn gp-btn--purple">Read more</span>
+					</div>
+					</a>
+				</div>
+
+
+				<div class="col-md-4 jobs">
+					<a href="https://gymplus.ie/jobs/lifeguard/">
+					<div class="job-card">
+						<h6 class="normal purple">
+							<i class="fa fa-location-arrow" aria-hidden="true"></i>
+							All Locations
+						</h6>
+						<h5>Lifeguard </h5>						<h6 class="normal">€ POA</h6>
+						<span class="gp-btn gp-btn--purple">Read more</span>
+					</div>
+					</a>
+				</div>
+
+
+				<div class="col-md-4 jobs">
+					<a href="https://gymplus.ie/jobs/swimming-instructor/">
+					<div class="job-card">
+						<h6 class="normal purple">
+							<i class="fa fa-location-arrow" aria-hidden="true"></i>
+							All Locations
+						</h6>
+						<h5>Swimming Instructor</h5>						<h6 class="normal">€ POA</h6>
+						<span class="gp-btn gp-btn--purple">Read more</span>
+					</div>
+					</a>
+				</div>
+									<div class="clearfix hidden-xs hidden-sm"></div>
+
+
+				<div class="col-md-4 jobs">
+					<a href="https://gymplus.ie/jobs/fitness-instructor/">
+					<div class="job-card">
+						<h6 class="normal purple">
+							<i class="fa fa-location-arrow" aria-hidden="true"></i>
+							All Locations
+						</h6>
+						<h5>Fitness Instructor</h5>						<h6 class="normal">€ POA</h6>
+						<span class="gp-btn gp-btn--purple">Read more</span>
+					</div>
+					</a>
+				</div>
+
+
+
+		</div>
+	</section>
  <?php
 require "footer.php";
   ?>
