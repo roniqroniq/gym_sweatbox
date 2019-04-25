50786abcb85eeacf1d93f7a58c9feea32534085a
Owen Lynch <owenlynch1310@gmail.com>
2019-04-25 21:32:34 +0100

    Update classes.php

--- classes.php
+++ classes.php
@@ -128,6 +128,26 @@ require "header.php";
         </div>
       </div>
 </div>
+<script>
+  $( window ).on( "load", function() {
+      mainNav();
+      $(window).scroll(function() {
+         mainNav();
+      });
+      function mainNav() {
+          var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+         if (top >1) $('.navy').stop().animate({
+            "opacity": '0',
+              "top": '0'
+           });
+         else $('.navy').stop().animate({
+              "top": '1',
+             "opacity": '1'
+         });
+
+       }
+       });
+  </script>
 
 </body>
  <?php
