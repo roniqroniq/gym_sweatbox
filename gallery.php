adf04ea4e4db5fc3a78c2abb7fff398207758a92
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-27 18:49:22 +0100

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -9,48 +9,62 @@ require "header.php";
         your muscles are hard. Damn right, my muscles are hard. I could train ya, but I’d have to charge.”</h2>
     </div>
   </div>
+  <div class="us"style="color:black;padding:7%;letter-spacing.7em;font-size:120%;">
   <div class="gallery" id="gallery">
-  <h3><br>Yoga / Boxingyoga</h3>
+    <div class="container">
+      <h3>Yoga / Boxingyoga</h3>
+    </div>
+
   <div class="row">
 
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/yoga1.jpeg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/yoga2.jpg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/yoga3.jpg"></div>
   </div>
+  <div class="container">
   <h3><br>Kettlebell</h3>
+  </div>
   <div class="row">
 
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/kettlebell1.jpg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/kettlebell2.jpg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/kettlebell3.jpg"></div>
   </div>
+  <div class="container">
   <h3><br>Spin Classes</h3>
+  </div>
   <div class="row">
 
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/girl_bike.jpg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/riding2.jpg"></div>
-    <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/riding3.jpg"></div>>
+    <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/riding3.jpg"></div>
   </div>
+  <div class="container">
   <h3><br>Weight training</h3>
+  </div>
   <div class="row">
 
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/weight1.jpg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/girl_back3.png"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/weight3.jpg"></div>
   </div>
+  <div class="container">
   <h3><br>Pilates</h3>
+  </div>
   <div class="row">
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/pilates1.jpg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/pilates2.jpg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/pilates3.jpg"></div>
   </div>
+  <div class="container">
   <h3><br>Boxing</h3>
+  </div>
   <div class="row">
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/boxing1.jpg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/boxing2.jpg"></div>
     <div class="col-sm-4 center"><img class="img-fluid img-thumbnail" src="img/boxing3.jpg"></div>
   </div>
-
+</div>
   </div>
 </div>
  </main>
