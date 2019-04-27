abb2fa0b6f288a29ffc23434c0e780995705fc04
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-04-27 22:22:22 +0100

    Update about.php

--- about.php
+++ about.php
@@ -3,7 +3,7 @@ include "header.php";
 ?>
 <html>
 <body>
-
+<main>
   <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
      <div class="container">
        <h2>“Those who think they have not time for bodily exercise will sooner or later have to find time for illness.”</h2>
@@ -14,7 +14,7 @@ include "header.php";
    	<div class="under-border under-border--opaic" style="background: #CFDE00"></div>
 
     <div class="container">
-      <h3> Welcome! <br> If you’re looking for the best gym in Dublin look no further – we have what you need. </h3>
+      <h3> Welcome! <br> We want to be the best, to do that our employees must be the best. <br> So they deserve to be treated that way. </h3>
     </div>
 		<div class="row pd-t pd-t--md">
 
@@ -26,86 +26,79 @@ include "header.php";
   In addition to this we also offer nutritional advise and hold monthly and weekly talks ranging from supplements to nutrion best practices.For more information check out our <a href="events.php">events page</a> or drop in to reception.
   We offer a range of plans for all levels and learners as well as additional packages , for more information about our prices check out our <a href="memberships.php">memberships page</a>.
 
-</p>
-<img src="">
 </div>
-<br>
-<br>
-<section class="services pt-100 pb-50" id="services">
-         <div class="container">
-            <div class="row">
-               <div class="col-xl-6 mx-auto text-center">
-                  <div class="section-title mb-100"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
-                     <h2>WHAT WE OFFER</h2>
-                   <br>
-                   <br>
-                   <br>
+</div>
+  <section class="services pt-100 pb-50" id="services">
+           <div class="container">
+              <div class="row">
+                 <div class="col-xl-6 mx-auto text-center">
+                    <div class="section-title mb-100"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                       <h2>WHAT WE OFFER</h2>
+                     <br>
+                     <br>
+                     <br>
 
-                  </div>
-               </div>
-            </div>
-
-
-            <div class="row">
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
-                     <i class="fas fa-building fa-5x"></i>
-                     <h4>3 FLOORS  </h4>
-                     <p>At sweat box we have three floors and each floor containing different sections to train</p>
-
-                  </div>
-
-               </div>
-               <br>
-               <br>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
-                  <i class="fa fa-check fa-5x"></i>
-                     <h4> EQUIPMENT </h4>
-                     <p>At sweat box we have world class equipment that is maintained and updated every year. </p>
-                  </div>
-               </div>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
-                  <i class="fa fa-anchor fa-5x"></i>
-
-                     <h4>POOL</h4>
-                     <p>We have a 25 meter pool as well as a steam room ,sauna and jacuzzi </p>
-                  </div>
-               </div>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
-                  <i class="fa fa-heart fa-5x"></i>
-                     <h4>MINDFULLNESS</h4>
-                     <p>We have a special mindfullness area where you can go to meditate and take time for you </p>
-                  </div>
-               </div>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
-                  <i class="fa fa-user fa-5x"></i>
-                     <h4>INSTRUCTORS</h4>
-                     <p> Fully trained instructors in both strenght and conditioning and nutrition</p>
-                  </div>
-               </div>
-               <div class="col-lg-4 col-md-6">
-                  <!-- Single Service -->
-                  <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
-                  <i class="fa fa-calendar fa-5x"></i>
-                     <h4>PLAN</h4>
-                     <p>Not only will you get a personailsed excersise plan you will also get a nutrional review<p>
-               </div>
-            </div>
-         </div>
-         <div class="text-center">
-    					<a href="#" class="btn btn-dark" style="background-color:grey">JOIN</a>
+                    </div>
+                 </div>
               </div>
-      </section>
-      </div>
+
+
+              <div class="row">
+                 <div class="col-lg-4 col-md-6">
+                    <!-- Single Service -->
+                    <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                       <i class="fas fa-building fa-5x"></i>
+                       <h4>3 FLOORS  </h4>
+                       <p>At sweat box we have three floors and each floor containing different sections to train</p>
+
+                    </div>
+
+                 </div>
+                 <br>
+                 <br>
+                 <div class="col-lg-4 col-md-6">
+                    <!-- Single Service -->
+                    <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                    <i class="fa fa-check fa-5x"></i>
+                       <h4> EQUIPMENT </h4>
+                       <p>At sweat box we have world class equipment that is maintained and updated every year. </p>
+                    </div>
+                 </div>
+                 <div class="col-lg-4 col-md-6">
+                    <!-- Single Service -->
+                    <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                    <i class="fa fa-anchor fa-5x"></i>
+
+                       <h4>POOL</h4>
+                       <p>We have a 25 meter pool as well as a steam room ,sauna and jacuzzi </p>
+                    </div>
+                 </div>
+                 <div class="col-lg-4 col-md-6">
+                    <!-- Single Service -->
+                    <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                    <i class="fa fa-heart fa-5x"></i>
+                       <h4>MINDFULLNESS</h4>
+                       <p>We have a special mindfullness area where you can go to meditate and take time for you </p>
+                    </div>
+                 </div>
+                 <div class="col-lg-4 col-md-6">
+                    <!-- Single Service -->
+                    <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                    <i class="fa fa-user fa-5x"></i>
+                       <h4>INSTRUCTORS</h4>
+                       <p> Fully trained instructors in both strenght and conditioning and nutrition</p>
+                    </div>
+                 </div>
+                 <div class="col-lg-4 col-md-6">
+                    <!-- Single Service -->
+                    <div class="single-service"style="padding:20%;text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">
+                    <i class="fa fa-calendar fa-5x"></i>
+                       <h4>PLAN</h4>
+                       <p>Not only will you get a personailsed excersise plan you will also get a nutrional review<p>
+                 </div>
+              </div>
+           </div>
+
 <br>
 <br>
 <div class="container">
@@ -117,82 +110,43 @@ include "header.php";
 <div class="container">
   <div class="row">
     <!-- Team Member 1 -->
-    <div class="col-xl-3 col-md-6 mb-4">
+    <div class="col-xl-4 col-md-6 mb-4">
       <div class="card border-0 shadow">
         <img src="https://source.unsplash.com/TMgQMXoglsM/500x350" class="card-img-top" alt="...">
         <div class="card-body text-center">
-          <h5 class="card-title mb-0">Team Member</h5>
+          <h5 class="card-title mb-0">Miriam</h5>
           <div class="card-text text-black-50">Personal Trainer</div>
         </div>
       </div>
     </div>
     <!-- Team Member 2 -->
-    <div class="col-xl-3 col-md-6 mb-4">
+    <div class="col-xl-4 col-md-6 mb-4">
       <div class="card border-0 shadow">
         <img src="https://source.unsplash.com/9UVmlIb0wJU/500x350" class="card-img-top" alt="...">
         <div class="card-body text-center">
-          <h5 class="card-title mb-0">Team Member</h5>
+          <h5 class="card-title mb-0">Owen</h5>
           <div class="card-text text-black-50">Nutritionist</div>
         </div>
       </div>
     </div>
     <!-- Team Member 3 -->
-    <div class="col-xl-3 col-md-6 mb-4">
+    <div class="col-xl-4 col-md-6 mb-4">
       <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/sNut2MqSmds/500x350" class="card-img-top" alt="...">
+        <img src="img/yana.png" class="card-img-top" alt="...">
         <div class="card-body text-center">
-          <h5 class="card-title mb-0">Team Member</h5>
+          <h5 class="card-title mb-0">Yana</h5>
           <div class="card-text text-black-50">Wellness Coach</div>
         </div>
       </div>
     </div>
-    <!-- Team Member 4 -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-0 shadow">
-        <img src="https://source.unsplash.com/ZI6p3i9SbVU/500x350" class="card-img-top" alt="...">
-        <div class="card-body text-center">
-          <h5 class="card-title mb-0">Team Member</h5>
-          <div class="card-text text-black-50">Experience Manager</div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- /.row -->
+
 
 </div>
 <!-- /.container -->
 
 <br>
 <br>
-
-<div id="googleMap" style="width:100%;height:400px;"></div>
-
-<script>
-function myMap() {
-var mapProp= {
-  center:new google.maps.LatLng(51.508742,-0.120850),
-  zoom:5,
-};
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-}
-</script>
-
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB8MsuFZxiXyp1AxgYaf7rOP-_x56Zq2MA&callback=myMap"></script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</main>
 
     <script>
 
