6211cf2c24d7da3128a682bd5b8d2217beb0a64d
Owen Lynch <owenlynch1310@gmail.com>
2019-05-06 16:12:39 +0100

    Add files via upload

---
+++ headermem.php
@@ -0,0 +1,98 @@
+
+<?php session_start();?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+    <link rel="stylesheet" type="text/css" href="theme.css">
+    <link href="css/caro.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+
+    <link href="https://fonts.googleapis.com/css?family=Archivo:500" rel="stylesheet">
+<body>
+
+<!-- Navigation -->
+<div class="navy">
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top">
+        <a class="navbar-brand" href="index.php" style="max-width: 4%;">
+                <img src="logo.png" class="img-fluid">
+             </a>
+
+
+          <!-- links toggle -->
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#links" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+
+          <div class="collapse navbar-collapse" id="links">
+            <ul class="navbar-nav mr-auto">
+              <!-- <li class="nav-item active">
+                <a class="nav-link" href="about.php">ABOUT</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="gallery.php">GALLERY</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="classes.php">CLASSES</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="testimonial.php">TESTIMONIALS</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="career.php">CAREERS</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="signup.php">JOIN NOW</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="contact.php">CONTACT US</a>
+              </li>
+
+              <li class="nav-item  active dropdown">
+                  <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    MEMBERSHIP
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="membership.php">LEVELS</a>
+                    
+                    
+                    <a class="dropdown-item" href="events.php">EVENTS</a>
+                  </div>
+                </li>
+            </ul>
+            </div> -->
+            
+            <p style="color:white;"> WELCOME <?php echo $_SESSION ['username'] ?> </p>
+            <div class="collapse navbar-collapse" id="account">
+            <ul class="navbar-nav ml-auto">
+               <br>
+               
+              <li class="nav-item active"><a class="nav-link"  href="index.php">LOG OUT</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+
+
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+      </body>
+</html>
---
+++ indexmem.php
@@ -0,0 +1,106 @@
+<?php
+include "headermem.php";
+?>
+
+
+
+<html>
+  
+    
+<body>  
+
+<div class="box" style="position: relative; left: 0; top: 0;">
+<img src="kettlebell.jpg" class="gym"/>
+    <img src="logo.png" class="log"/>
+</div>
+
+<br>
+
+<section>
+    <div class="container ">
+    <div class="row">
+    	<div class="card card border-0 shadow col-md-12 p-3 ">
+    		<div class="row ">
+    			<div class="col-md-4 ">
+    				<img class="w-100" src="aboutus.jpg" style ="width:500px;height:250px;margin:5%;">
+    			</div>
+    			<div class="col-md-8">
+    				<div class="card-body  mt-8">
+    					<h3 class="card-title text-center border-0"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">WELCOME TO YOUR GYM PAGE</h3>
+              <div class ="text-center">
+    					<p class="card-text "style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    					<a href="about.php" class="btn btn-dark"style="background-color:grey;colour:white">CHOOSE YOUR CLASSES</a>
+                 </div>   
+             </div>
+    			</div>
+         
+    		</div>
+        </div>
+        <br>
+        <div class="card border-0">
+    	<div class="card border-0 shadow col-md-12 p-3">
+      
+    		<div class="row ">
+            <div class="col-md-4">
+    				<img class="w-100" src="girls_jump.jpg"style ="width:500px;height:250px;margin:5%;">
+    			</div>
+    			<div class="col-md-8">
+    				<div class="card-body">
+    					<h3 class="card-title text-center"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">BOOK PERSONAL TRAINER</h3>
+              <div class="text-center">
+    					<p class="card-text"style="text-align:center;font-family: 'Roboto', sans-serif,!important;color:grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    					<a href="signup.php" class="btn btn-dark" style="background-color:grey">BOOK NOW</a>
+                   
+              </div>
+    				</div>
+    			</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<br>
+<br>
+
+
+<br>
+<div class="jumbotron jumbotron-fluid"style="font-family: 'Roboto', sans-serif,!important;color:grey">
+  <div class="container">
+    <h1 class="display-4">"IF YOU FEEL LIKE STOPPING , THINK ABOUT WHY YOU STARTED."</h1>
+   
+  </div>
+</div>
+</div>
+</div>
+
+<script>
+
+$( window ).on( "load", function() {
+    mainNav();
+    $(window).scroll(function() {
+       mainNav();
+    });
+    function mainNav() {
+        var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+       if (top > 600) $('.navy').stop().animate({
+          "opacity": '0',
+            "top": '0'
+         });
+       else $('.navy').stop().animate({
+            "top": '1',
+           "opacity": '1'
+       });
+
+ }
+     });
+   
+
+
+
+</script>
+</body>
+</html>
+
+<?php
+include "footer.php";
+?>
