b96d28f3885275928c65232bc633622d58796729
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-05-09 08:04:06 +0100

    Update loginScript.php

--- loginScript.php
+++ loginScript.php
@@ -5,7 +5,7 @@ session_start();
 <?php
 
 if(isset($_POST['login'])){
-    
+
    $username = $_POST['email'];
    $userpassword = $_POST['password'];
 
@@ -14,17 +14,17 @@ if(isset($_POST['login'])){
 
 
 
-   //SQL query to check the email and password of the user are in the database 
+   //SQL query to check the email and password of the user are in the database
 $query = "SELECT * FROM user WHERE user_email ='{$username}'";
  $select_user_query =mysqli_query($db_connection,$query);
  if (!$select_user_query){
      die("Query failed".mysqli_error($db_connection));
  }
-//looping through the info in the database to see if it is there 
+//looping through the info in the database to see if it is there
    while ($row =mysqli_fetch_array($select_user_query)){
 
  $db_username = $row['user_email'];
- 
+
  $db_password= $row['user_password'];
  $db_userrole= $row['role_id'];
    }
