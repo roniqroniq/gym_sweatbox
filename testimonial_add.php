c622526faa87cd42eb6e0653a6b23ed6bca149a8
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-05-10 15:44:14 +0100

    Create testimonial_add.php

---
+++ testimonial_add.php
@@ -0,0 +1,102 @@
+<?php
+require "header.php";
+ ?>
+
+<div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
+    <div class="container">
+      <h2>“Sweat is your fat crying.”</h2>
+    </div>
+  </div>
+
+ <main role="main">
+
+<div class="container">
+  <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Oscar Hunt (Silver Member):<span class="text-muted">"It’ll blow your mind.""</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5">
+        <div class="col-sm-4 center"<img class="img-fluid img-thumbnail" src="img/yoga1.jpeg"></div>
+        <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg> -->
+        <img src="img/man1.jpg" class="rounded float-left" alt="...">
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading"> Robert Reid (Gold Member): <span class="text-muted">"Oh yeah, it’s that good. See for yourself.""</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg> -->
+        <img src="img/man2.jpg" class="rounded float-right" alt="...">
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Skylar Bird (First time visiting): <span class="text-muted">" The membership fees are very reasonable."</span></h2>
+        <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+      </div>
+      <div class="col-md-5">
+        <!-- <svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg> -->
+        <img src="img/man3.jpg" class="rounded float-left" alt="...">
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+
+    <!-- Upload Testimonial. -->
+
+    <div class="container">
+    <div class="row">
+        <div class="col-md-6 col-md-offset-3">
+        <form action="upload-manager.php" method="post" enctype="multipart/form-data">
+            <h4>Have a feedback? Upload your pictures and share your comments with the others! </h4>
+            <h6> Only .pdf, .jpg and .jpeg formats allowed. <br>
+            Max size: 5 MB. </h6>
+        </div>
+        <div class="col-md-6 col-md-offset-3">
+            <label for="fileSelect">Filename:</label>
+            <input type="file" name="photo" id="fileSelect">
+            <input type="submit" name="submit" value="Upload">
+        </div>
+      </div>
+      </div>
+
+    <!-- Upload Testimonial. -->
+</div>
+    <!-- /END THE FEATURETTES -->
+    <script>
+  $( window ).on( "load", function() {
+      mainNav();
+      $(window).scroll(function() {
+         mainNav();
+      });
+      function mainNav() {
+          var top = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+         if (top >1) $('.navy').stop().animate({
+            "opacity": '0',
+              "top": '0'
+           });
+         else $('.navy').stop().animate({
+              "top": '1',
+             "opacity": '1'
+         });
+
+       }
+       });
+  </script>
+  </div><!-- /.container -->
+</main>
+ <?php
+require "footer.php"
+  ?>
