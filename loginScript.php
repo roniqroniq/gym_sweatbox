e616b4b311ff83dc50c5856f94e39b217f35c41c
Owen Lynch <owenlynch1310@gmail.com>
2019-05-10 20:39:56 +0100

    Update loginScript.php

--- loginScript.php
+++ loginScript.php
@@ -18,7 +18,7 @@ if(isset($_POST['login'])){
 
 
     //SQL query to check the email and password of the user are in the database 
-    $query = "SELECT * FROM member WHERE user_email ='{$username}'";
+    $query = "SELECT * FROM user WHERE user_email ='{$username}'";
     $select_user_query =mysqli_query($db_connection,$query);
     if (!$select_user_query){
         die("Query failed".mysqli_error($db_connection));
@@ -28,8 +28,8 @@ if(isset($_POST['login'])){
     while ($row =mysqli_fetch_array($select_user_query)){
 
         $db_username = $row['user_email'];
-        $db_password= $row['user_password'];
-        $db_userrole= $row['role_id'];
+        $db_password= $row['password'];
+        $db_userrole= $row['role'];
 
    }
 // This should redirect to the index page if the username and user password are not  the same . This is not working and I am not sure why.
@@ -40,7 +40,7 @@ if(isset($_POST['login'])){
 // This should redirect if the password and user email are the same .
 } else if ($username == $db_username && $userpassword == $db_password){
 
-    if ($db_userrole == 2 || $db_userrole == 0 ){//here if the user role is 2 then it will log the user in as normal user
+    if ($db_userrole == 2  ){//here if the user role is 2 then it will log the user in as normal user
         $_SESSION['username'] = $db_username;// here we are assigning this user name (email) to a session called username.
         $_SESSION['role_id'] = $db_userrole;
         $_SESSION['user_password'] = $db_password;
@@ -51,7 +51,7 @@ if(isset($_POST['login'])){
         $_SESSION['username'] = $db_username;
         $_SESSION['role_id'] = $db_userrole;
 
-        header ("Location:admin.php");
+        header ("Location:adminindex.php");
         exit;
     }
 }else {
