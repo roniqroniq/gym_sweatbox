a46e255e54b4602ea332d6dea69c093d804a5827
Miriam W <mirwalsh3@gmail.com>
2019-05-10 13:30:33 +0100

    gggy

--- membership.php
+++ membership.php
@@ -1,6 +1,20 @@
 <?php
 include "header.php";
-?>
+include('server_mem.php');
+
+if(isset($_GET['edit'])){
+  $id = $_GET['edit'];
+  $edit_state = true;
+  $rec =mysqli_query($db_connection, "SELECT * FROM fee_edit WHERE id=$id ");
+  $record = mysqli_fetch_array($rec);
+  $type = $record['type'];
+  $fee = $record['fee'];
+  $line1 = $record['line1'];
+  $line2 = $record['line2'];
+  $line3 = $record['line3'];
+  $id = $record['id'];
+}?>
+
 <body>
 <main>
   <div class="jumbotron jumbotron-fluid" style="background-image: url(img/gym-banner.jpg); background-size: cover; color: white;">
@@ -19,30 +33,34 @@ include "header.php";
   <div class="card-deck mb-3 text-center">
     <div class="card mb-4 shadow-sm">
       <div class="btnstyl">
-        <h4 class="my-0 font-weight-normal">Gold</h4>
+        <?php while ($row = mysqli_fetch_array($results)){ ?>
+        <h4 class="my-0 font-weight-normal"><?php echo $row['type']; ?></h4>
       </div>
       <div class="card-body">
-        <h1 class="card-title pricing-card-title">€ 50 <small class="text-muted"> /mo</small></h1>
+
+        <h1 class="card-title pricing-card-title">€ <?php echo $row['fee']; ?> <small class="text-muted"> /mo</small></h1>
         <ul class="list-unstyled mt-3 mb-4">
-          <li>Anytime access</li>
-          <li>Private Showers and Locker room</li>
-          <li>Choose any number of classes</li>
-          <li>Free Personal Trainer once per month</li>
+          <li><?php echo $row['line1']; ?></li>
+          <li><?php echo $row['line2']; ?></li>
+          <li><?php echo $row['line3']; ?></li>
+          <li><?php echo $row['line4']; ?></li>
         </ul>
         <a href="signup.php"  class="btn btn-lg btn-block btn-dark">Sign up for Gold</a>
       </div>
     </div>
+
     <div class="card mb-4 shadow-sm">
       <div class="btnstyl2">
-        <h4 class="my-0 font-weight-normal">Silver</h4>
+        <h4 class="my-0 font-weight-normal"><?php echo $row['type']; ?></h4>
       </div>
       <div class="card-body">
-        <h1 class="card-title pricing-card-title">€ 35 <small class="text-muted">/ mo</small></h1>
+        <h1 class="card-title pricing-card-title">€  <?php echo $row['fee']; ?> <small class="text-muted">/ mo</small></h1>
         <ul class="list-unstyled mt-3 mb-4">
-          <li>Anytime access</li>
-          <li>Private Showers and Locker room</li>
-          <li>Choose 3 classes</li>
-          <li>Free Personal Trainer once per month</li>
+          <li><?php echo $row['line1']; ?></li>
+          <li><?php echo $row['line2']; ?></li>
+          <li><?php echo $row['line3']; ?></li>
+          <li><?php echo $row['line4']; ?></li>
+        </ul><?php } ?>
         </ul>
         <a href="signup.php"  class="btn btn-lg btn-block btn-dark">Sign up for Silver</a>
       </div>
@@ -62,8 +80,11 @@ include "header.php";
         <a href="signup.php"  class="btn btn-lg btn-block btn-dark btn-mem">Sign up for Bronze</a>
       </div>
     </div>
-  </div>
-</div>
+    </div>
+    </div>
+
+
+
 
 
   <script>
