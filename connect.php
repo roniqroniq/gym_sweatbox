16879bac8a76fe39f9721cf719fd794cd8367159
Yana Tudzharova <https://yanatudzharova@bitbucket.org/yanatudzharova/yanatudzharova.git>
2019-05-28 22:23:32 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$db_connection = mysqli_connect('127.0.0.1', 'root', '', 'sweatbox');
+$db_connection = mysqli_connect('127.0.0.1', 'root', '', 'sweatbox', 3306);
 mysqli_set_charset($db_connection, 'utf8');
 
 ?>
